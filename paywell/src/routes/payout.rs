8581c1c9ca23ecd065377c3c8cfb50b60f2def74
Wilfred007 <adzerwilfred007@gmail.com>
2025-09-15 15:54:35 +0100

    code opt

--- paywell/src/routes/payout.rs
+++ paywell/src/routes/payout.rs
@@ -1,24 +1,3 @@
-// use axum::{Json, extract::State};
-// use sqlx::MySqlPool;
-// use crate::models::models::{CreatePayout, Transaction};
-// use crate::services::payout_services::initiate_payout;
-// use crate::config::Config;
-// use crate::routes::auth;
-// use crate::routes::auth_middleware::AuthUser;
-// // use crate::routes::auth_middleware::AuthUser;
-
-// pub async fn create_payout(
-//     State(db): State<MySqlPool>,
-//     AuthUser { user_id }: AuthUser,
-//     Json(payload): Json<CreatePayout>,
-// ) -> Json<Transaction> {
-//     let config = Config::from_env();
-//     let tx = initiate_payout(&db, payload, &user_id, &config.paystack_secret)
-//         .await
-//         .unwrap();
-//     Json(tx)
-// }
-
 
 use axum::{Json, extract::State};
 use std::sync::Arc;
@@ -29,7 +8,7 @@ use crate::routes::auth_middleware::AuthUser;
 use crate::AppState;
 
 pub async fn create_payout(
-    State(app_state): State<Arc<AppState>>,   // 👈 Use Arc<AppState>
+    State(app_state): State<Arc<AppState>>,   // 
     AuthUser { user_id }: AuthUser,
     Json(payload): Json<CreatePayout>,
 ) -> Json<Transaction> {
